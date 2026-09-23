0f678d20090c21407591d3ac27911e244d57be01
Riley <97197314+Riley-Truong@users.noreply.github.com>
2026-09-23 11:49:16 -0400

    TESTING LINK

--- fibc-react-project/src/components/Navigation.jsx
+++ fibc-react-project/src/components/Navigation.jsx
@@ -27,7 +27,7 @@ const NAV_GROUPS = [
 const NAV_LINKS = [
     { to: '/ministries', label: 'Ministries' },
     { to: '/sermons', label: 'Sermons' },
-    { to: '#', label: 'Give'},
+    { to: 'https://faithbaptistmcdonoughchurch.netlify.app/', label: 'Give'},
     { to: '/events', label: 'Events' },
     { to: '/contact', label: 'Contact' },
 ];
